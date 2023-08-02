17b1bad7aaf7c9d53a8ffae267d3dbef26a09f08
einerpino25 <einerpinocabrera@gmail.com>
2023-08-02 16:04:17 -0500

    feat: :art: einer

---
+++ src/components/productos/DetallesProductos.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-page class="q-pa-md">
+    <q-container>
+      <q-card class="q-mb-md">
+        <q-card-section>
+          <div class="text-h5">Detalles ingresados:</div>
+          <div>Nombre: {{ detalles.nombre }}</div>
+          <div>Consumible: {{ detalles.consumible }}</div>
+          <div>Stock Total: {{ detalles.stockTotal }}</div>
+          <div>Descripción: {{ detalles.descripcion }}</div>
+          <div>Código de Barra: {{ detalles.codigoBarra }}</div>
+          <div>Categoría: {{ detalles.categoria }}</div>
+        </q-card-section>
+      </q-card>
+    </q-container>
+  </q-page>
+</template>
+
+<script>
+export default {
+  props: ["detalles"],
+};
+</script>
+
+<style>
+/* Estilos personalizados */
+/* ... */
+</style>
---
+++ src/pages/ProductosRegistro.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>Productos</div>
+  <ProductosForm />
+</template>
+
+<script setup>
+import ProductosForm from "components/productos/productosForm.vue";
+</script>
+
+<style lang="scss" scoped></style>
