f3029fe97b025a5721307acdb003c685207b247a
Jose Zapata Granda <jdzg04@gmail.com>
2023-08-10 16:42:31 -0500

    feat: entering the cards and the chart

---
+++ src/components/dashboard/TarjetaEstad.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-card class="my-card">
+    <q-card-section class="tarjeta">
+      <div class="flex justify-center q-mb-lg">
+        <q-icon :name="icono" size="24px" :color="iconColor"></q-icon>
+      </div>
+      <div class="flex justify-between items-center text-grey">
+        <div class="text-h6 text-grey-8">{{ titulo }}</div>
+        <div class="text-subtitle2 align-center">{{ subtitulo }}</div>
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script setup>
+const props = defineProps({
+  titulo: String,
+  subtitulo: String,
+  icono: String,
+  iconColor: String,
+});
+</script>
+
+<style lang="scss" scoped>
+// .tarjeta {
+//   height: 120px;
+//   width: 116px;
+// }
+</style>
