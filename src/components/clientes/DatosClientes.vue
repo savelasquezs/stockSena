5a8881c5269a9ecb99b5560b1e50087ae6abd6c7
Joan Zapata <zapatajoan05@gmail.com>
2023-08-15 08:17:13 -0500

    feat: MOdifique el login

--- src/components/clientes/DatosClientes.vue
+++ src/components/clientes/DatosClientes.vue
@@ -1,44 +1,14 @@
 <template>
-  <div class="flex justify-evenly content-center complete items-center">
-    <q-page class="flex content-center">
-      <div class="q-gutter-md q-mx-lg">
-        <q-select
-          outlined
-          label="Tipo documento"
-          v-model="Tipo_documento"
-          :options="options_Tipo_documento"
-          transition-show="flip-up"
-          transition-hide="flip-up"
-          style="width: 250px"
-        />
-
-        <q-btn
-          -btn
-          type="submit"
-          label="Iniciar sesión"
-          color="green-14"
-          style="width: 250px"
-        />
-        <q-btn
-          type="submit"
-          label="Inir"
-          color="green-14"
-          style="width: 250px"
-        />
+  <div>
+    <div class="q-pa-lg">
+      <h4>Detalles</h4>
+      <div>
+        <div class="row q-pa-lg">
+          <p style="col: 40px" class="col">Nombres</p>
+          <p class="col">Joan</p>
+          <p></p>
+        </div>
       </div>
-    </q-page>
+    </div>
   </div>
 </template>
-
-<script>
-import AutocompleteInput from "components/utils/autocompleteInput.vue";
-import autocompleteinput from "components/utils/autocompleteInput.vue";
-export default {
-  data() {
-    return {
-      Tipo_documento: null,
-      options_Tipo_documento: ["SEbas", "Joan"],
-    };
-  },
-};
-</script>
