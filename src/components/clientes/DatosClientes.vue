3b8c2f55e74502ef9330304a9c53c414bc600bc8
Joan Zapata <zapatajoan05@gmail.com>
2023-08-16 08:18:15 -0500

    feat: modifique datos clientes

--- src/components/clientes/DatosClientes.vue
+++ src/components/clientes/DatosClientes.vue
@@ -1,14 +1,71 @@
 <template>
-  <div>
-    <div class="q-pa-lg">
-      <h4>Detalles</h4>
-      <div>
-        <div class="row q-pa-lg">
-          <p style="col: 40px" class="col">Nombres</p>
-          <p class="col">Joan</p>
-          <p></p>
+  <div class="client-info-card">
+    <q-card class="q-pa-md">
+      <q-card-section class="text-h6 q-flex justify-between items-center">
+        Información del Cliente
+        <q-btn class="" color="primary" label="Editar" />
+      </q-card-section>
+      <q-card-section> </q-card-section>
+      <q-card-section>
+        <div class="q-gutter-md">
+          <div class="q-mb-md">
+            <strong>Nombre:</strong><span class="info-client">Joan</span>
+            <!-- {{ cliente.nombre }} -->
+          </div>
+          <div class="q-mb-md">
+            <strong>Apellido:</strong>
+            <span class="info-client">Zapata</span>
+            <!-- {{ cliente.apellido }} -->
+          </div>
+          <div class="q-mb-md">
+            <strong>Docuemnto:</strong
+            ><span class="info-client">1027800932</span>
+            <!-- {{ cliente.apellido }} -->
+          </div>
+          <div class="q-mb-md">
+            <strong>Estado:</strong><span class="info-client">Morosos</span>
+            <!-- {{ cliente.apellido }} -->
+          </div>
+
+          <!-- Agrega más campos de información del cliente aquí -->
         </div>
-      </div>
-    </div>
+      </q-card-section>
+    </q-card>
   </div>
 </template>
+
+<script setup>
+// import { ref, props } from "vue";
+
+// const cliente = props.cliente;
+</script>
+
+<style scoped>
+.client-info-card {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  padding: 50px;
+}
+
+.q-card {
+  max-width: 100%;
+  width: 100%;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+}
+
+.q-card-section {
+  padding: 20px;
+}
+
+.q-gutter-md > * {
+  margin-bottom: 20px;
+}
+
+.text-h6 {
+  font-size: 1.25rem;
+  font-weight: 500;
+  margin-bottom: 20px;
+}
+</style>
--- src/components/clientes/OpcionesClientes.vue
+++ src/components/clientes/OpcionesClientes.vue
@@ -1,5 +1,7 @@
 <template>
-  <q-item-section style="background-color: #f5f5f5">
+  <div style="background-color: #f5f5f5">
+    <!-- Contenido aquí -->
+
     <div class="flex justify-evenly q-mt-lg">
       <q-card class="my-card q-px-lg">
         <q-icon
@@ -50,73 +52,253 @@
       <tarjeta-estad />
     </div>
 
-    <div>
-      <q-table
-        :rows="tableData"
-        :columns="tableColumns"
-        row-key="id"
-        class="my-sticky-header-table q-mt-lg"
-        flat
-        bordered
-        table-header-style="background-color:#00af00; color:#ffff"
-        virtual-scroll
-        :rows-per-page-options="[0]"
-      >
-      </q-table>
+    <div class="q-pa-md">
+      <div class="q-pa-md row items-start q-gutter-md flex justify-center">
+        <q-item clickable to="/"> </q-item>
+        <StadisticTableItem
+          titulo="TOTAL PRODUCTOS"
+          valor="34.000"
+          periodo="ultima semana"
+          text-color="text-orange"
+        />
+        <StadisticTableItem
+          titulo="LO MÁS PRESTADO"
+          valor="6.890"
+          periodo="ultima semana"
+          text-color="text-green"
+        />
+        <StadisticTableItem
+          titulo="BAJAS"
+          valor="4.000"
+          periodo="ultima semana"
+          text-color="text-red-14"
+        />
+      </div>
+      <div class="q-tables">
+        <div class="q-mr-lg">
+          <div>
+            <q-input class="q-ml-sm" v-model="filtro" style="width: 410px" />
+          </div>
+          <div class="flex justify-end">
+            <div>
+              <q-btn
+                @click="toggleVentanaEmergente"
+                label="Agregar Producto"
+                icon="add_circle_outline"
+                color="primary"
+                style="width: 210px"
+                class="q-ml-sm"
+              />
+              <q-btn
+                @click="exportTable"
+                icon="file_download"
+                color="primary"
+                label="Descargar tabla"
+                class="q-ml-sm"
+                style="width: 210px"
+              >
+              </q-btn>
+            </div>
+          </div>
+        </div>
+        <div class="q-tables">
+          <q-dialog v-model="mostrarVentanaEmergente">
+            <q-card>
+              <q-card-section>
+                <productos-form />
+              </q-card-section>
+
+              <q-card-actions>
+                <q-btn
+                  dense
+                  flat
+                  round
+                  icon="close"
+                  @click="toggleVentanaEmergente"
+                />
+              </q-card-actions>
+            </q-card>
+          </q-dialog>
+          <q-table
+            style="height: 500px"
+            flat
+            bordered
+            :rows="rows"
+            :columns="columns"
+            row-key="index"
+            :rows-per-page-options="[0]"
+            v-model="pagination"
+            virtual-scroll
+            :filter="filtro"
+            class="my-card flex shadow-5 shadow-up-3"
+            table-header-style="background-color:#00af00; color:#ffff; shadow-n"
+          >
+            <template v-slot:body-cell-acciones="props">
+              <q-td :props="props">
+                <q-btn
+                  @click="toggleVentanaEmergente"
+                  icon="visibility"
+                  rounded
+                  size="10px"
+                  style="width: 20px; margin-right: 8px"
+                  text-color="blue-5"
+                />
+                <q-btn
+                  icon="edit"
+                  rounded
+                  size="10px"
+                  style="width: 20px; margin-right: 8px"
+                  text-color="secondary"
+                />
+                <q-btn
+                  icon="delete_forever"
+                  rounded
+                  size="10px"
+                  style="width: 20px; margin-right: 8px"
+                  text-color="red-8"
+                />
+              </q-td>
+            </template>
+          </q-table>
+        </div>
+      </div>
     </div>
-  </q-item-section>
+  </div>
 </template>
 
-<script setup>
-const tableColumns = [
-  {
-    name: "id",
-    required: true,
-    label: "ID",
-    align: "left",
-    field: "id",
-    sortable: true,
-  },
-  {
-    name: "codigo",
-    required: true,
-    label: "Codigo",
-    align: "left",
-    field: "codigo",
-    sortable: true,
-  },
-  {
-    name: "producto",
-    required: true,
-    label: "Producto",
-    align: "left",
-    field: "producto",
-    sortable: true,
-  },
-  {
-    name: "fecha_de_pedido",
-    required: true,
-    label: "Fecha de pedido",
-    align: "left",
-    field: "Fecha de pedido",
-    sortable: true,
-  },
-];
+<style scoped>
+.q-tables {
+  padding: 5px;
+  background-color: #f5f5f5;
+}
+.search-bar {
+  display: flex;
+  align-items: center;
+  border: 2px solid #ccc;
+  border-radius: 24px;
+  padding: 3px;
+  margin-bottom: 5px;
+  max-width: 700px;
+  margin: 0 auto;
+  padding-right: 8px;
+}
 
-const tableData = [
-  { id: 1, name: "John Doe", age: 30 },
-  { id: 2, name: "Jane Smith", age: 25 },
-  { id: 3, name: "Michael Johnson", age: 40 },
-];
-</script>
+.search-icon,
+.clear-icon {
+  padding: 6px;
+  cursor: pointer;
+}
 
-<script>
-import SearchBar from "components/utils/SearchBar.vue";
-import TarjetaEstad from "../dashboard/TarjetaEstad.vue";
-</script>
-
-<style>
-.bg-primary {
-  background-color: #1976d2; /* Cambia el color a tu preferencia */
+.search-input {
+  border: none;
+  flex: 1;
+  padding: 6px;
+  background-color: #f5f5f5;
+  outline: none;
 }
 </style>
+
+<script setup>
+import { defineComponent } from "vue";
+import { QDialog, QCard, QCardSection, QCardActions, QBtn } from "quasar";
+
+import { ref } from "vue";
+import { db } from "../../firebaseInit";
+import StadisticTableItem from "../utils/StadisticTableItem.vue";
+
+import { exportFile } from "quasar";
+
+const filtro = ref("");
+
+function toggleVentanaEmergente() {
+  mostrarVentanaEmergente.value = !mostrarVentanaEmergente.value;
+}
+
+const exportTable = () => {
+  const columnLabels = columns.value.map((col) => col.label);
+  const dataRows = rows.value.map((row) =>
+    columns.value.map((col) =>
+      wrapCsvValue(
+        typeof col.field === "function"
+          ? col.field(row)
+          : row[col.field === void 0 ? col.name : col.field],
+        col.format,
+        row
+      )
+    )
+  );
+
+  const content = [columnLabels].concat(dataRows);
+  const csvContent = content
+    .map((row) => row.map((cell) => wrapCsvValue(cell)).join(";"))
+    .join("\r\n");
+
+  // Codificar el contenido en UTF-8 y agregar el BOM
+  const encodedContent = new TextEncoder().encode("\uFEFF" + csvContent);
+
+  const blob = new Blob([encodedContent], {
+    type: "text/csv;charset=UTF-8",
+  });
+
+  // Descarga el archivo CSV utilizanseado Quasar exportFile
+  exportFile("table-export.csv", blob);
+};
+
+const wrapCsvValue = (val, formatFn, row) => {
+  let formatted = formatFn !== void 0 ? formatFn(val, row) : val;
+
+  formatted =
+    formatted === void 0 || formatted === null ? "" : String(formatted);
+
+  return formatted;
+};
+
+const columns = ref([
+  {
+    name: "name",
+    required: true,
+    label: "Nombre",
+    align: "left",
+    field: (row) => row.name,
+    format: (val) => `${val}`,
+    sortable: true,
+  },
+
+  {
+    name: "Consumible",
+    align: "center",
+    label: "Consumible",
+    field: "consumable",
+    sortable: true,
+  },
+  {
+    name: "Stock Total",
+    label: "Stock Total",
+    field: "totalStock",
+    sortable: true,
+  },
+
+  {
+    name: "Código de barra",
+    label: "Código de barra",
+    field: "barCode",
+  },
+
+  { name: "Stock-Prestamo", label: "Stock-Prestamo", field: "totalStock" },
+
+  { name: "Almacen", label: "Almacen", field: "almacen" },
+  { name: "acciones", label: "Acciones", field: "acciones" },
+]);
+
+const rows = ref([]);
+
+const tableRef = collection(db, "products");
+onSnapshot(tableRef, (snapshot) => {
+  snapshot.docChanges().forEach((change) => {
+    if (change.type == "added") {
+      rows.value.push(change.doc.data());
+      console.log(rows.value);
+    }
+  });
+});
+</script>
