5bee4a65b64850189e8065abb3bcebab382b3aa7
Santiago Velásquez Serna <santyvano@outlook.com>
2023-10-23 16:00:05 -0500

    feat:

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,60 +2,12 @@
   <QDialogo v-model="modalPrestamoOpened" iconModal="real_estate_agent">
     <PrestamosFormVue @prestamoGuardado="modalPrestamoOpened = false" />
   </QDialogo>
-
   <QDialogo v-model="Codigo" iconModal="view_week">
     <BarcodeGenerator />
   </QDialogo>
   <QDialogo v-model="modalDevolverOpened" iconModal="real_estate_agent">
     <DevolverPrestamo @devuelto="modalDevolverOpened = false" />
   </QDialogo>
-  <div>
-    <div class="ticket">
-      <img src="public\img\Sena.png" alt="Logo" />
-      <p class="centered">
-        RECEIPT EXAMPLE <br />Address line 1 <br />Address line 2
-      </p>
-      <table>
-        <thead>
-          <tr>
-            <th class="quantity">Q.</th>
-            <th class="description">Description</th>
-            <th class="price">$$</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td class="quantity">1.00</td>
-            <td class="description">ARDUINO UNO R3</td>
-            <td class="price">$25.00</td>
-          </tr>
-          <tr>
-            <td class="quantity">2.00</td>
-            <td class="description">JAVASCRIPT BOOK</td>
-            <td class="price">$10.00</td>
-          </tr>
-          <tr>
-            <td class="quantity">1.00</td>
-            <td class="description">STICKER PACK</td>
-            <td class="price">$10.00</td>
-          </tr>
-          <tr>
-            <td class="quantity"></td>
-            <td class="description">TOTAL</td>
-            <td class="price">$55.00</td>
-          </tr>
-        </tbody>
-      </table>
-      <p class="centered">Thanks for your purchase! <br />parzibyte.me/blog</p>
-    </div>
-    <button
-      id="btnPrint"
-      class="hidden-print"
-      @click="imprimirTicketConAcentos"
-    >
-      Print
-    </button>
-  </div>
 
   <q-layout view="hHh lpR fFf" class="page-background-gray">
     <q-header elevated>
